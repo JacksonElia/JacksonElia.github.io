a3f6e58c0e2d1c135b4969504ac91cf792b94434
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-06-19 16:51:38 -0400

    Added logic and code for Random Projects

--- scripts/random-project.js
+++ scripts/random-project.js
@@ -1,6 +1,47 @@
-let randomProjectPage = document.getElementsByClassName("page-section")[0] // Theres only one on the page
+let randomProjects = document.getElementsByClassName("random-project-div")
 
-function loadRandomProject() {
-
+function showProject(projectNumber) {
+  let randomProject = randomProjects[projectNumber]
+  randomProject.style.display = "block";
+  randomProject.style.visibility = "visible";
 }
 
+function loadPage() {
+  let projectName = window.location.hash;
+
+  console.log(projectName)
+
+  switch (projectName) {
+    case "#AFK_Apex_Bot":
+      showProject(1);
+      break;
+    case "#Bouncy":
+      showProject(2);
+      break;
+    case "#COC_Auto_Bot":
+      showProject(3);
+      break;
+    case "#DCS_Terminal":
+      showProject(4);
+      break;
+    case "#Ethermine_Tracker":
+      showProject(5)
+      break;
+    case "#Mazer":
+      showProject(6);
+      break;
+    case "#Screen_Dimmer":
+      showProject(7);
+      break;
+    case "#William_Whitakers_Words_App":
+      showProject(8);
+      break;
+    case "#This_Website":
+      showProject(9);
+      break;
+    default:
+      showProject(Math.ceil(Math.random() * 9));
+  }
+}
+
+loadPage();
