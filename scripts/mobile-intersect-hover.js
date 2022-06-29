7efd7872d06904d15ee58444a16ca0ed7f6bccfa
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-06-28 20:20:06 -0400

    Made hover effects work for mobile

--- scripts/mobile-intersect-hover.js
+++ scripts/mobile-intersect-hover.js
@@ -1,6 +1,44 @@
-function isElementInMiddleOfScreen(element) {
-  let element_rect = element.getBoundingClientRect();
-  if (1.3 * element_rect.y >= 2 * window.innerHeight / 4 && .7 * (element_rect.y + element_rect.height) <= 3 * window.innerHeight / 4) {
-    console.log("in middle")
+let buttonsPresent = true;
+let galleryPresent = true;
+
+function checkButtons() {
+  let buttons = document.getElementsByClassName("button1");
+  for (let button of buttons) {
+    focusCenterElement(button);
   }
 }
+
+function checkGallery() {
+  let cells = document.getElementsByClassName("cell");
+  for (let cell of cells) {
+    focusCenterElement(cell);
+  }
+}
+
+function focusCenterElement(element) {
+  let element_rect = element.getBoundingClientRect();
+  if (element_rect.y + element_rect.height / 2 >= window.innerHeight / 3 &&
+      element_rect.y + element_rect.height / 2 <= 2 * window.innerHeight / 3) {
+    element.focus();
+    console.log("focus");
+  } else {
+    element.blur();
+  }
+}
+
+function focusMainLoop() {
+  // Only works on mobile devices
+  if (screen.width < 1050) {
+    if (buttonsPresent) {
+      checkButtons();
+    }
+
+    if (galleryPresent) {
+      checkGallery();
+    }
+  }
+
+  requestAnimationFrame(focusMainLoop);
+}
+
+requestAnimationFrame(focusMainLoop);
